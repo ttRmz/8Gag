a7a1bf0d4a52e4b21d8011612739f6d50ccf15c8
eniotnam <marteantoine@gmail.com>
2017-02-14 14:29:50 +0100

    new login

--- login.php
+++ login.php
@@ -1,7 +1,8 @@
 <?php
-
 require 'connect.php';
-if (!empty($_GET)) {
+
+if (isset($_POST['connect'])){
+
     $req = $dbh->prepare('SELECT * FROM user 
                    WHERE email = :email 
                    AND password = :password');
@@ -11,48 +12,94 @@ if (!empty($_GET)) {
     ]);
     $users = $req->fetchAll();
     if (count($users) > 0) {
-        session_start();
         $_SESSION['connected'] = true;
         $_SESSION['id'] = $users[0]['id'];
-        header('Location:add_product.php');
-    } else {
-        header('location:inscription.php');
+        header('Location:index.php');
     }
 }
+
+if (isset($_POST['inscrit'])){
+    $pseudo = htmlspecialchars($_POST['name']);
+    $mail = htmlspecialchars($_POST['email']);
+
+
+    if(!empty($_POST['name']) && !empty($_POST['email']) 
+       && !empty($_POST['password']) && !empty($_POST['passwords']))
+    {
+
+
+        if($_POST['password']==$_POST['passwords'])
+        {
+            $requete = $dbh->prepare('INSERT INTO user VALUES(NULL,
+      :name, :password, :email
+    )');
+            $requete->execute([
+                ':name' => $_POST['name'],
+                ':password' => $_POST['password'],
+                ':email' => $_POST['email']
+            ]);
+            header('Location:login.php');
+        }
+        else
+        {
+            $erreur = "vos mot de passe ne sont pas identiques !";
+        }
+    }
+
+
+}
+
+
 ?>
-
-
-
-    <!doctype html>
-    <html lang="en">
+<!doctype html>
+<html lang="en">
 
     <head>
-        <link rel="stylesheet" href="popup.css">
+
 
     </head>
 
     <body>
 
-        <div class="box">
-            <a class="button" href="#popup1">login</a>
+
+
+
+
+        <form method="post" action="">
+            <input type="text" id="email" name="email" placeholder="email" size=30 required>
+            <br>
+            <input type="text" type="password" id="password" name="password" placeholder="mot de passe" size=30 required>
+            <br>
+            <button  name="connect" type="submit" type="button" class="btn btn-primary">CONNECT</button>
+            <br>
+            <a  href="inscription.php"class="inscrire">s'inscrire </a>
+        </form>
+
+
+
+
+        <form method="post" action="">
+            <input type="text" id="name" name="name" placeholder="pseudo" size=30 required>
+            <br>
+            <input type="text" id="email" name="email" placeholder="email" size=30 required>
+            <br>
+            <input type="password" id="password" name="password" placeholder="mot de passe" size=30 required>
+            <br>
+            <input type="password" id="passwords" name="passwords" placeholder="confirmer mot de passe" size=30 required>
+            <br>
+            <button name="inscrit" type="submit">INSCRIT</button>
+            <br>
+            <?php
+            if(isset($erreur))
+            {
+                echo '<font color="red">'.$erreur.'</font>';
+            }
+            ?>
+        </form>
         </div>
 
-        <div id="popup1" class="overlay">
-            <div class="popup">
-               
-                <h2>login</h2>
-                <a class="close" href="#">&times;</a>
-                <div class="content">
-                  
-                    <form action="" method="">
-                        <input type="text" name="email" id="email" placeholder="email" size="30"><br>
-                        <input type="text" name="password" id="password" placeholder="password" size="30"><br>
-                        <button type="submit" value="Envoyer" size="30">Connect</button>
-                    </form>
-
-                </div>
-            </div>
-        </div>
-    </body>
-
-    </html>
+    </div>
+</div>
+</div>
+</body>
+</html>
