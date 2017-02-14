74d3318c8c6a59fead573c0d6e06ce9fd5c3db11
eniotnam <marteantoine@gmail.com>
2017-02-14 18:33:40 +0100

    sécurité + new version

--- login.php
+++ login.php
@@ -1,105 +1,172 @@
 <?php
+
 require 'connect.php';
 
-if (isset($_POST['connect'])){
-
-    $req = $dbh->prepare('SELECT * FROM user 
-                   WHERE email = :email 
-                   AND password = :password');
-    $req->execute([
-        ':email' => $_POST['email'],
-        ':password' => $_POST['password']
-    ]);
-    $users = $req->fetchAll();
-    if (count($users) > 0) {
-        $_SESSION['connected'] = true;
-        $_SESSION['id'] = $users[0]['id'];
-        header('Location:index.php');
-    }
-}
-
-if (isset($_POST['inscrit'])){
-    $pseudo = htmlspecialchars($_POST['name']);
-    $mail = htmlspecialchars($_POST['email']);
-
-
-    if(!empty($_POST['name']) && !empty($_POST['email']) 
-       && !empty($_POST['password']) && !empty($_POST['passwords']))
+if(isset($_GET['formconnexion']))
+{
+    $mailconnect = $_GET['mailconnect'];
+    $mdpconnect = $_GET['mdpconnect'];
+    if(!empty($mailconnect) && !empty($mdpconnect) )
     {
+        $mailconnect = htmlspecialchars($_GET['mailconnect']);
+        $mdpconnect = sha1($_GET['mdpconnect']);
+
+        $requser = $dbh->prepare("SELECT * FROM user WHERE email = ? AND password = ? ");
+        $requser->execute(array($mailconnect, $mdpconnect));
 
 
-        if($_POST['password']==$_POST['passwords'])
+        $userexist = $requser->rowCount();
+        if($userexist == 1)
         {
-            $requete = $dbh->prepare('INSERT INTO user VALUES(NULL,
-      :name, :password, :email
-    )');
-            $requete->execute([
-                ':name' => $_POST['name'],
-                ':password' => $_POST['password'],
-                ':email' => $_POST['email']
-            ]);
-            header('Location:login.php');
+            $userinfo = $requser->fetchALL();
+            $_SESSION['id'] = $userinfo[0]['id'];
+            $_SESSION['pseudo'] = $userinfo['pseudo'];
+            $_SESSION['mail'] = $userinfo['mail'];
+            $_SESSION['connected'] = true;
+            header("Location: a.php");
         }
         else
         {
-            $erreur = "vos mot de passe ne sont pas identiques !";
+            $erreur = "Mauvais mail ou mot de passe incorrect !";
         }
     }
-
-
+    else
+    {
+        $erreur = "Tous les champs doivent être complétés !";
+    }
 }
 
+if (isset($_GET['forminscription']))
+{ $pseudo = htmlspecialchars($_GET['pseudo']);
+ $mail = htmlspecialchars($_GET['mail']);
+ $mdp = sha1($_GET['mdp']);
+ $mdp2 = sha1($_GET['mdp2']);
+
+ if(!empty($_GET['pseudo']) && !empty($_GET['mail']) 
+    AND !empty($_GET['mdp']) && !empty($_GET['mdp']))
+ {
+
+     if(filter_var($mail, FILTER_VALIDATE_EMAIL ))
+     {
+
+         $reqmail = $dbh->prepare("SELECT * FROM user WHERE mail = ? ");
+         $reqmail->execute(array($mail));
+         $mailexist= $reqmail->rowCount();
+         if($mailexist==0)
+         {
+
+
+             if($mdp==$mdp2)
+             {
+                 $insertmbr =$dbh->prepare ("INSERT INTO user (name, email, password) VALUES (?, ?, ? ) ");
+                 $insertmbr -> execute (array($pseudo, $mail, $mdp));
+                 $erreurs = "Votre compte a été bien crée ! veuillez vous connecter";
+             }
+             else
+             {
+                 $erreurs = "vos mot de passe ne sont pas identiques !";
+             }
+         }
+         else {
+             $erreurs = "Adresse mail déjà utilisée !";
+         }
+     }
+     else
+     {
+         $erreurs = "Votre adresse mail n'est pas valide !";
+     }
+ }
+
+ else
+ {
+     $erreurs = "tous les champs doivent être complétés !";
+ }
+}
 
 ?>
-<!doctype html>
-<html lang="en">
 
+<html>
     <head>
+        <title>MyPressePerso </title>
+        <meta charset="utf-8">
+        <link href="css/connexion.css" rel="stylesheet">
+        <link href="css/inscription.css" rel="stylesheet"
+              </head>
+        <body>
+            <div align="center">
+
+                <form method="" action="" class="mix">
+                    <h2>Connexion</h2>
+                    <br/><br/>
+                    <input type="email" name="mailconnect" placeholder="Mail"/>
+                    <input type="password" name="mdpconnect" placeholder="Votre mot de passe"/>
+                    <input type="submit" name="formconnexion" value="Se connecter"/>
+                </form>
+                <?php
+                if(isset($erreur))
+                {
+                    echo '<font color="red">'.$erreur.'</font>';
+                }
+                ?>
+                <div align="center">
+                    <form method="" action="" class="form">
+                        <h2>Inscription</h2>
+                        <br/><br/>
+                        <table>
+                            <tr>
+                                <td align="right">
+                                    <label for="pseudo">Pseudo </label>
+                                </td>
+                                <td>
+                                    <input type="text" name="pseudo" id="pseudo" placeholder="votre pseudo" class="textbox" value="<?php if(isset($pseudo)) { echo $pseudo;} ?>">
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td align="right">
+                                    <label for="mail">Mail </label>
+                                </td>
+                                <td>
+                                    <input type="email" name="mail" id="mail" placeholder="votre mail" class="textbox" value="<?php if(isset($mail)) { echo $mail;} ?>">
+                                </td>
+                            </tr>
 
 
-    </head>
 
-    <body>
+                            <tr>
+                                <td align="right">
+                                    <label for="mdp">Mot de passe </label>
+                                </td>
+                                <td>
+                                    <input type="password" name="mdp" id="mdp" placeholder="votre mot de passe" class="textbox">
+                                </td>
+                            </tr>
 
+                            <tr>
+                                <td align="right">
+                                    <label for="mdp2">Confirmer votre mot de passe</label>
+                                </td>
+                                <td>
+                                    <input type="password" name="mdp2" id="mdp2" placeholder="confirmer votre mdp" class="textbox">
+                                </td>
+                            </tr>
 
+                            <tr>
+                                <td></td>
+                                <td align="center">
+                                    <br/>
+                                    <input type="submit" value="Je m'inscris" name="forminscription" class="button">
+                                </td>
+                            </tr>
+                        </table>
+                    </form>
+                    <?php
+                    if(isset($erreurs))
+                    {
+                        echo '<font color="red">'. $erreurs.'</font>';
+                    }
+                    ?>
+                </div>
 
-
-
-        <form method="post" action="">
-            <input type="text" id="email" name="email" placeholder="email" size=30 required>
-            <br>
-            <input type="text" type="password" id="password" name="password" placeholder="mot de passe" size=30 required>
-            <br>
-            <button  name="connect" type="submit" type="button" class="btn btn-primary">CONNECT</button>
-            <br>
-            <a  href="inscription.php"class="inscrire">s'inscrire </a>
-        </form>
-
-
-
-
-        <form method="post" action="">
-            <input type="text" id="name" name="name" placeholder="pseudo" size=30 required>
-            <br>
-            <input type="text" id="email" name="email" placeholder="email" size=30 required>
-            <br>
-            <input type="password" id="password" name="password" placeholder="mot de passe" size=30 required>
-            <br>
-            <input type="password" id="passwords" name="passwords" placeholder="confirmer mot de passe" size=30 required>
-            <br>
-            <button name="inscrit" type="submit">INSCRIT</button>
-            <br>
-            <?php
-            if(isset($erreur))
-            {
-                echo '<font color="red">'.$erreur.'</font>';
-            }
-            ?>
-        </form>
-        </div>
-
-    </div>
-</div>
-</div>
-</body>
-</html>
+                </body>
+            </html>
