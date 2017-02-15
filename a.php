5ed90d0543199a3c650f77c8435940819fa6ae24
eniotnam <marteantoine@gmail.com>
2017-02-15 10:36:08 +0100

    bloque bouton categorie si non connect

--- a.php
+++ a.php
@@ -1,12 +1,25 @@
 <?php
 require 'connect.php';
 
+
   if (isset($_SESSION['connected'])){
             echo '<a href="logout.php">logout</a>';
         }
         else {
             echo '<a href="login.php">Login/register</a>';
         }
-    
+
+  if(isset($_SESSION['connected'])){
+      echo '<br> <a href="logout.php">log</a>';
+  }
+else{
+    echo '<br> <a onclick="myFunction()" >log<p id="demo"></p></a>
+            ';
+}
 ?>
 
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "<div style='color:red'>PAS INSCRIT</div>";
+}
+</script>
