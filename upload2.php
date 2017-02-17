e29de048c9c9a4217779091e6d07ee07485b43de
ttRmz <tristan.rami@gmail.com>
2017-02-18 00:26:22 +0100

    upload2 correct

--- upload2.php
+++ upload2.php
@@ -1,4 +1,3 @@
-
 <?php
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
@@ -6,6 +5,10 @@ require 'connect.php';
 
 // L'utilisateur a envoyé l'image
 if (!empty($_FILES)) {
+    $radio=$_POST['cat'];
+    $text=$_POST['name'];
+    $nomimg=$_FILES['pic']['name'];
+
     $mime_valid = ['image/png', 'image/jpeg','image/gif'];
     $extension_valid = ['png', 'jpeg','jpg','gif'];
     $extension = pathinfo($_FILES['pic']['name'])['extension'];
@@ -15,53 +18,48 @@ if (!empty($_FILES)) {
     if(in_array($extension, $extension_valid) && in_array($mime, $mime_valid)){
         move_uploaded_file($_FILES['pic']['tmp_name'], 'uploads/' . $_FILES['pic']['name']);
         echo 'Done';
+
+
+        $stmt = $dbh->prepare('SELECT id FROM categories WHERE name=:name');
+        $arg=[
+            ':name' => $radio
+        ];
+        $stmt->execute($arg);
+
+        while ($result=$stmt->fetch()){
+            global $id_cat;
+            $id_cat=$result['id'];
+        }
+        $stmt1 = $dbh->prepare('INSERT INTO pictures(id, name , picture, categories_id ,user_id) VALUES(NULL, :name, :picture, :cat, :user)');
+        $arg1=[
+            ':name' => $text,
+            ':picture' => $nomimg,
+            ':cat' => $id_cat,
+            ':user' => 5
+
+        ];
+        $stmt1->execute($arg1);
     } else {
         echo 'Erreur de format';
     }
 }
-if (!empty($_FILES)) {
-    $radio=$_POST['cat'];
-    $text=$_POST['name'];
-
-    $stmt = $dbh->prepare('SELECT id FROM categories WHERE name=:name');
-    $arg=[
-        ':name' => $radio
-    ];
-    $stmt->execute($arg);
-
-    while ($result=$stmt->fetch()){
-        global $id_cat;
-        $id_cat=$result['id'];
-    }
-
-    $stmt = $dbh->prepare('
-                INSERT INTO pictures(id, name , picture, categories_id,user_id) 
-                VALUES (null, :name, :picture ,:cat, :user);');
-    $arg=[
-        ':name' => $text,
-        ':picture' => $_FILES['pic']['name'],
-        ':cat' => $id_cat,
-        ':user' => $_SESSION['id']
-
-    ];
-    $stmt->execute($arg);
-
-}
 ?>
 <form method="POST" enctype="multipart/form-data">
-    <!-- Limite du fichier à 100Ko -->
-    <input type="hidden" name="MAX_FILE_SIZE" value="100000">
-    <input type="file" name="pic">
+    <input type="file" name="pic"><br>
     <input type="text" name="name" value="Picture's Name"><br>
-    <input type="radio" name="cat" value="Food" > Food<br>
-    <input type="radio" name="cat" value="City"> City<br>
-    <input type="radio" name="cat" value="Mode" > Mode<br>
-    <input type="radio" name="cat" value="Portrait"> Portrait<br>
-    <input type="radio" name="cat" value="Sport" > Sport<br>
-    <input type="radio" name="cat" value="Landscape"> Landscape<br>
-    <input type="radio" name="cat" value="Animals"> Animals <br>
-    <input type="radio" name="cat" value="Music" > Music<br>
-    <input type="radio" name="cat" value="Others" checked > Others <br>
-    <input type="submit" name="Submit" value="Submit">
+    <select name="cat">
+        <option value="Food" > Food </option><br>
+        <option value="City"> City</option><br>
+        <option value="Mode" > Mode</option><br>
+        <option value="Portrait"> Portrait</option><br>
+        <option value="Sport" > Sport</option><br>
+        <option value="Landscape"> Landscape</option><br>
+        <option value="Animals"> Animals </option><br>
+        <option value="Music" > Music</option><br>
+        <option value="Others" > Others </option><br>
+    </select>
+    <button type="submit">
+        Envoyer
+    </button>
 
 </form>
